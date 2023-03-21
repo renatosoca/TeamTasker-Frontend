831dc5e1d5c51ae9de68b156a2fb8c11bb5e3114
DevRenato <u18215194@gmail.com>
2023-03-20 20:31:20 -0500

    projects slice

---
+++ src/store/project/projectSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const projectSlice = createSlice({
+  name: 'project',
+  initialState: {
+    projects: undefined,
+    project: undefined,
+  },
+  reducers: {
+  },
+});
+
+export const {  } = projectSlice.actions;
